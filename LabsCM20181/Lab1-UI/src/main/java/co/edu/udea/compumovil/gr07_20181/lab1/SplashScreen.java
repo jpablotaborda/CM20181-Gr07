9847690ccfeba67b82698617c0af38970a52a124
jpablotaborda <jpablo.taborda@udea.edu.co>
2018-02-23 18:36:40 -0500

    Splash screen terminado

---
+++ LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20181/lab1/SplashScreen.java
@@ -0,0 +1,16 @@
+package co.edu.udea.compumovil.gr07_20181.lab1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashScreen.this,Menurest.class));
+        finish();
+    }
+}
