1150c54c63a390c8fb9d9469f61506ec32ab5165
jpablotaborda <jpablo.taborda@udea.edu.co>
2018-02-23 17:36:24 -0500

    Primer push

---
+++ LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20181/lab1/Menurest.java
@@ -0,0 +1,13 @@
+package co.edu.udea.compumovil.gr07_20181.lab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Menurest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menurest);
+    }
+}
