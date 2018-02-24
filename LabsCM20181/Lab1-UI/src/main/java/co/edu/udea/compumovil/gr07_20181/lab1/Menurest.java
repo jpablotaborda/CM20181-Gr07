fb78d67d7030a8fb5560c31c667398df2408d123
jpablotaborda <jpablo.taborda@udea.edu.co>
2018-02-23 19:19:34 -0500

    Implementandos Intents para navegación del menu principal

--- LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20181/lab1/Menurest.java
+++ LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20181/lab1/Menurest.java
@@ -1,13 +1,35 @@
 package co.edu.udea.compumovil.gr07_20181.lab1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Menurest extends AppCompatActivity {
+public class Menurest extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menurest);
+        Button bot_platos=(Button) findViewById(R.id.bot_platos);
+        Button bot_bebidas=(Button) findViewById(R.id.bot_bebidas);
+
+        bot_platos.setOnClickListener(this);
+        bot_bebidas.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bot_platos:
+                Intent lanzar_platos= new Intent(getApplicationContext(),ven_platos.class);
+                startActivity(lanzar_platos);
+                break;
+            case R.id.bot_bebidas:
+                Intent lanzar_bebidas= new Intent(getApplicationContext(),ven_bebidas.class);
+                startActivity(lanzar_bebidas);
+                break;
+        }
     }
 }
