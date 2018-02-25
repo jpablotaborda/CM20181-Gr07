1531ccd11ba5523b21c90a5a955051212c3c2651
jpablotaborda <jpablo.taborda@udea.edu.co>
2018-02-25 16:03:45 -0500

    Implementando parte gráfica de registro de platos

--- LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20181/Activities/ven_platos.java
+++ LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20181/Activities/ven_platos.java
@@ -5,6 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +18,7 @@ import co.edu.udea.compumovil.gr07_20181.Adapters.Adaptaplatos;
 import co.edu.udea.compumovil.gr07_20181.Models.Plato;
 import co.edu.udea.compumovil.gr07_20181.lab1.R;
 
-public class ven_platos extends AppCompatActivity {
+public class ven_platos extends AppCompatActivity implements View.OnClickListener {
 
     private RecyclerView recyclerplatos;
     private List<Plato> lista_platos;
@@ -21,6 +26,8 @@ public class ven_platos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ven_platos);
+
+        //configurando recyclerview
         iniciar_lista();
         recyclerplatos= (RecyclerView)findViewById(R.id.recyclerplatos);
         NestedScrollView scrollpla= (NestedScrollView) findViewById(R.id.scrollplatos);
@@ -33,14 +40,43 @@ public class ven_platos extends AppCompatActivity {
         Adaptaplatos adap_recycler_platos= new Adaptaplatos(lista_platos);
         recyclerplatos.setAdapter(adap_recycler_platos);
 
+        // Implementando métodos del formulaio registro de platos
+
+        Button bot_regplato= (Button) findViewById(R.id.bot_regplato);
+        Button sel_tiempre= (Button) findViewById(R.id.bot_tiempre_platoreg);
+        Button sel_foto= (Button) findViewById(R.id.bot_foto_platoreg);
+        bot_regplato.setOnClickListener(this);
+        sel_foto.setOnClickListener(this);
+        sel_foto.setOnClickListener(this);
+
+
     }
 
     private void iniciar_lista() {
         lista_platos= new ArrayList<>();
-        lista_platos.add(new Plato(R.drawable.rancheros,"Frijoles Rancheros","20/02/2018","Plato Fuerte","1:30H","12.000$","- Frijoles, - Salchicha Ranchera, - Zanahoria, -Platano, -Chicharron"));
-        lista_platos.add(new Plato(R.drawable.rancheros,"Frijoles Rancheros","20/02/2018","Plato Fuerte","1:30H","12.000$","- Frijoles, - Salchicha Ranchera, - Zanahoria, -Platano, -Chicharron"));
-        lista_platos.add(new Plato(R.drawable.rancheros,"Frijoles Rancheros","20/02/2018","Plato Fuerte","1:30H","12.000$","- Frijoles, - Salchicha Ranchera, - Zanahoria, -Platano, -Chicharron"));
-        lista_platos.add(new Plato(R.drawable.rancheros,"Frijoles Rancheros","20/02/2018","Plato Fuerte","1:30H","12.000$","- Frijoles, - Salchicha Ranchera, - Zanahoria, -Platano, -Chicharron"));
+        lista_platos.add(new Plato(R.drawable.rancheros,"Frijoles Rancheros","Mañana","Plato Fuerte","1:30H","12.000$","- Frijoles, - Salchicha Ranchera, - Zanahoria, -Platano, -Chicharron"));
+        lista_platos.add(new Plato(R.drawable.rancheros,"Frijoles Rancheros","Mañana","Plato Fuerte","1:30H","12.000$","- Frijoles, - Salchicha Ranchera, - Zanahoria, -Platano, -Chicharron"));
+        lista_platos.add(new Plato(R.drawable.rancheros,"Frijoles Rancheros","Mañana","Plato Fuerte","1:30H","12.000$","- Frijoles, - Salchicha Ranchera, - Zanahoria, -Platano, -Chicharron"));
+        lista_platos.add(new Plato(R.drawable.rancheros,"Frijoles Rancheros","Mañana","Plato Fuerte","1:30H","12.000$","- Frijoles, - Salchicha Ranchera, - Zanahoria, -Platano, -Chicharron"));
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bot_regplato:
+
+                EditText nom_plato= (EditText) findViewById(R.id.edit_nomreg);
+                CheckBox checkmañana= (CheckBox) findViewById(R.id.horario_platocheckmanana);
+                CheckBox checktarde= (CheckBox) findViewById(R.id.horario_platochecktarde);
+                CheckBox checknoche= (CheckBox) findViewById(R.id.horario_platochecknoche);
+                RadioButton entrada=  (RadioButton) findViewById(R.id.tipoplatocheckentrada);
+                RadioButton platofuerte=  (RadioButton) findViewById(R.id.tipoplatocheckplafuerte);
+                EditText precio_pla= (EditText) findViewById(R.id.edit_precioreg);
+                EditText ingredientes_pla= (EditText) findViewById(R.id.edit_ingredientesreg);
+
+                break;
+
+        }
     }
 }
