fa8383b332bed8a4790779a8e5716ec399a81446
jpablotaborda <jpablo.taborda@udea.edu.co>
2018-02-26 14:10:26 -0500

    Haciendo validaciones y implmentado seleccionar foto de galeria

--- LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20181/Activities/ven_platos.java
+++ LabsCM20181/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr07_20181/Activities/ven_platos.java
@@ -1,5 +1,8 @@
 package co.edu.udea.compumovil.gr07_20181.Activities;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +13,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +26,8 @@ public class ven_platos extends AppCompatActivity implements View.OnClickListene
 
     private RecyclerView recyclerplatos;
     private List<Plato> lista_platos;
+    private static final int SELECCIONAR_IMAGEN=100;
+    private Uri Image_uri=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,7 +52,7 @@ public class ven_platos extends AppCompatActivity implements View.OnClickListene
         Button sel_tiempre= (Button) findViewById(R.id.bot_tiempre_platoreg);
         Button sel_foto= (Button) findViewById(R.id.bot_foto_platoreg);
         bot_regplato.setOnClickListener(this);
-        sel_foto.setOnClickListener(this);
+        sel_tiempre.setOnClickListener(this);
         sel_foto.setOnClickListener(this);
 
 
@@ -74,9 +80,58 @@ public class ven_platos extends AppCompatActivity implements View.OnClickListene
                 RadioButton platofuerte=  (RadioButton) findViewById(R.id.tipoplatocheckplafuerte);
                 EditText precio_pla= (EditText) findViewById(R.id.edit_precioreg);
                 EditText ingredientes_pla= (EditText) findViewById(R.id.edit_ingredientesreg);
+                //comprobando que los datos ingresados no estén vacios.
 
+                if(nom_plato.getText().toString().isEmpty()){
+                    Toast.makeText( getApplicationContext(),"Debe ingresar el nombre del plato",Toast.LENGTH_SHORT).show();
+
+                }
+                else if(!checkmañana.isChecked() && !checktarde.isChecked() && !checknoche.isChecked()){
+                    Toast.makeText( getApplicationContext(),"Debe seleccionar un horario para el plato",Toast.LENGTH_SHORT).show();
+                }
+                else if(!entrada.isChecked()&& !platofuerte.isChecked()){
+                    Toast.makeText( getApplicationContext(),"Debe seleccionar un tipo de plato",Toast.LENGTH_SHORT).show();
+
+                }
+                else if(precio_pla.getText().toString().isEmpty()){
+                    Toast.makeText( getApplicationContext(),"Debe ingresar un precio para el plato",Toast.LENGTH_SHORT).show();
+
+                }
+                else if(Image_uri==null){
+                    Toast.makeText( getApplicationContext(),"Debe seleccionar una imagen para el plato",Toast.LENGTH_SHORT).show();
+
+                }
+                else if(ingredientes_pla.getText().toString().isEmpty()){
+                    Toast.makeText( getApplicationContext(),"Ingrese los ingredientes plato",Toast.LENGTH_SHORT).show();
+
+                }
+
+                else// si se validaron con exito todos los datos
+                {
+
+
+                }
+
+                break;
+            case R.id.bot_tiempre_platoreg:
+
+
+                break;
+            case R.id.bot_foto_platoreg:
+                Intent lanzar_galeria= new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(lanzar_galeria,SELECCIONAR_IMAGEN);
                 break;
 
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode ==RESULT_OK && requestCode==SELECCIONAR_IMAGEN){
+            Image_uri=data.getData();
+
+        }
+
     }
 }
